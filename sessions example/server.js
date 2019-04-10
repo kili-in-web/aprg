1019aa9e1c57cfefd1e9d918b2b580e0bbafb255
Andreas Plaß <andreas.plass@haw-hamburg.de>
2019-04-10 10:06:34 +0200

    Session-Beispiel

--- sessions example/server.js
+++ sessions example/server.js
@@ -22,3 +22,25 @@ app.use(session({
 	saveUninitialized: true
 }));
 
+// Sessionvariable setzen
+app.get('/sessionSetzen', function(req, res){
+	req.session['sessionValue'] = Math.floor(Math.random()*100);
+	res.redirect('/index');
+});
+
+// Sessionvariable löschen
+app.get('/sessionLoeschen', function(req, res){
+	delete req.session['sessionValue'];
+	res.redirect('/index');
+});
+
+// Sessionvariable anzeigen
+app.get('/index', function(req, res){
+	if (!req.session['sessionValue']){
+		res.render('index', {message: "Sessionvariable nicht gesetzt"});
+	}
+	else{
+		res.render('index', {message: `Wert der Sessionvariable:
+				${req.session['sessionValue']}`});
+	}
+});
